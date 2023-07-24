72d56e69c733866516520d51785fbcb640b68ba7
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-07-24 09:37:33 +0200

    dev runs ok

--- app/router.js
+++ app/router.js
@@ -4,7 +4,6 @@ const app = express();
 const router = express.Router();
 
 //routes list here
-
 // router.get("/quickview", authentication.verifyToken, get_controller.getAllInfosForMyEnterprise);
-
+//
 export default router;
