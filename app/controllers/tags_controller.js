ff66ddc17f7ef102a5d658595a942e480d82a2ad
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-07-25 21:38:06 +0200

    add tag route is ok

--- app/controllers/tags_controller.js
+++ app/controllers/tags_controller.js
@@ -1,44 +1,44 @@
 import tags_model from "../models/tags_model.js";
 
 const tags_controller = {
-	async Alltags(req, res) {
-		try {
-			let tags = await tags_model.getAlltags();
-			if (tags) {
-				res.json(tags);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async AlltagsFromOnePost(req, res) {
-		try {
-			const postId = req.params.id;
-			let tags = await tags_model.getAlltagsFromOnePost(postId);
-			if (tags) {
-				res.json(tags);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async AddTags(req, res) {
-		try {
-			const { name } = req.body;
-			let tagAdded = await tags_model.addTags(name);
-			if (tagAdded) {
-				res.json(tagAdded);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
+    async Alltags(req, res) {
+        try {
+            let tags = await tags_model.getAlltags();
+            if (tags) {
+                res.json(tags);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async AlltagsFromOnePost(req, res) {
+        try {
+            const postId = req.params.id;
+            let tags = await tags_model.getAlltagsFromOnePost(postId);
+            if (tags) {
+                res.json(tags);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async AddTags(req, res) {
+        try {
+            const { name } = req.body.data;
+            let tagAdded = await tags_model.AddTag(name);
+            if (tagAdded) {
+                res.json(tagAdded);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
 };
 
 export default tags_controller;
