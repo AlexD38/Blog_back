92ec6ee9571978c144064b2808780b4f711dbe1e
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-08-06 14:33:58 +0200

    added tags into get posts route

--- app/controllers/posts_controller.js
+++ app/controllers/posts_controller.js
@@ -1,64 +1,72 @@
 import posts_model from "../models/posts_model.js";
 
 const posts_controller = {
-	async AllPosts(req, res) {
-		try {
-			let posts = await posts_model.getAllPosts();
-			if (posts) {
-				res.json(posts);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async OnePost(req, res) {
-		try {
-			const postId = req.params.id;
-			let post = await posts_model.getOnePost(+postId);
-			if (post) {
-				res.json(post);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async AddPost(req, res) {
-		try {
-			const { title, slug, body } = req.body;
-			console.log(title, slug, body);
-			let newPost = await posts_model.addPost(title, slug, body);
-			if (newPost) {
-				res.status(200).json({ success: "Post has been added" });
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async EditPost(req, res) {
-		try {
-			const { title, slug, body } = req.body;
-			const id = req.params.id;
+    async AllPosts(req, res) {
+        try {
+            let posts = await posts_model.getAllPosts();
+            if (posts) {
+                res.json(posts);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async AllPostsFromTheirTags(req, res) {
+        try {
+            const tagId = req.params.tagId;
+            let posts = await posts_model.getAllPostsFromTheirTags(tagId);
+            if (posts) {
+                res.json(posts);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async OnePost(req, res) {
+        try {
+            const postId = req.params.id;
+            let post = await posts_model.getOnePost(+postId);
+            if (post) {
+                res.json(post);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async AddPost(req, res) {
+        try {
+            const { title, slug, body } = req.body;
+            console.log(title, slug, body);
+            let newPost = await posts_model.addPost(title, slug, body);
+            if (newPost) {
+                res.status(200).json({ success: "Post has been added" });
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async EditPost(req, res) {
+        try {
+            const { title, slug, body } = req.body;
+            const id = req.params.id;
 
-			console.log(title, slug, body);
-			let correctedPost = await posts_model.editPost(
-				title,
-				slug,
-				body,
-				id
-			);
-			if (correctedPost) {
-				res.status(200).json({ success: "Post has been updated" });
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
+            console.log(title, slug, body);
+            let correctedPost = await posts_model.editPost(title, slug, body, id);
+            if (correctedPost) {
+                res.status(200).json({ success: "Post has been updated" });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
 };
 
 export default posts_controller;
--- app/router.js
+++ app/router.js
@@ -12,20 +12,15 @@ router.get("/posts", posts_controller.AllPosts);
 router.get("/posts/:id(\\d+)", posts_controller.OnePost);
 router.get("/posts/:id(\\d+)/tags", tags_controller.AlltagsFromOnePost);
 router.get("/tags", tags_controller.Alltags);
+router.get("/tags/:id(\\d+)/posts", posts_controller.AllPostsFromTheirTags);
 router.post("/login", AuthMiddleware.verifyUser);
 router.post("/tags", tags_controller.AddTags);
 router.post("/posts/:id(\\d+)/tags", tags_controller.AttachTagsToPost);
-router.post(
-	"/posts/:id(\\d+)/existingtags",
-	tags_controller.AttachTagsToPostFromAnExistingTag
-);
+router.post("/posts/:id(\\d+)/existingtags", tags_controller.AttachTagsToPostFromAnExistingTag);
 router.post("/posts", posts_controller.AddPost);
 router.delete("/posts/:id(\\d+)", delete_controller.deleteRecord);
 router.delete("/tags/:id(\\d+)", delete_controller.deleteRecord);
-router.delete(
-	"/posts/:postId(\\d+)/tags/:tagId(\\d+)",
-	tags_controller.detachTagsToPost
-);
+router.delete("/posts/:postId(\\d+)/tags/:tagId(\\d+)", tags_controller.detachTagsToPost);
 router.patch("/posts/:id(\\d+)", posts_controller.EditPost);
 
 export default router;
