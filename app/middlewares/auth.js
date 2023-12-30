be4305c95725c486d59f7b8a2554833965c48452
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-12-30 15:58:01 +0100

    added auth jwt

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,9 +1,9 @@
 import user_model from "../models/user_model.js";
-
+import jwt from "jsonwebtoken";
+import env from "dotenv";
 const AuthMiddleware = {
     async verifyUser(req, res, next) {
-        // extraction de l'email et pwd depuis le body de la requete
-        const { mail, pwd } = req.body;
+        const { mail } = req.body;
         try {
             const mailFound = await user_model.getUserMail(mail);
             if (!mailFound) {
@@ -15,11 +15,45 @@ const AuthMiddleware = {
                 res.status(405).json({ error: "...Mauvais identifiant" });
                 return;
             }
-            res.status(200).json({ isAdmin: pwdFound[0].is_admin, userName: pwdFound[0].user_name });
-            console.log(pwdFound[0]);
+            // console.log(pwdFound[0]);
+            res.locals.user = { isAdmin: pwdFound[0].is_admin, userName: pwdFound[0].user_name };
+            next();
         } catch (error) {
             console.log(error);
             return;
+        }
+    },
+    async createToken(req, res) {
+        try {
+            const token = jwt.sign(
+                {
+                    exp: Math.floor(Date.now() / 1000) + 60 * 60,
+                    data: res.locals.user,
+                },
+                process.env.SECRET
+            );
+            res.status(200).json({ token, userName: res.locals.user.userName });
+            // console.log("token : ", token);
+            // const decodedToken = jwt.verify(token, "secret");
+            // console.log("token decoded : ", decodedToken);
+        } catch (error) {
+            throw new Error("could not create token", error);
+        }
+    },
+    async verifyToken(req, res, next) {
+        const token = req.headers.token;
+        // console.log("token sent from frontend", token);
+        try {
+            if (!token) {
+                res.status(404).json({ message: "there's no token to verify" });
+                return;
+            }
+            const decodedToken = jwt.verify(token, process.env.SECRET);
+            console.log(decodedToken);
+            next();
+        } catch (error) {
+            console.error("Error verifying token:", error);
+            res.status(401).json({ message: "Invalid token" });
         }
     },
 };
--- app/models/categories_model.js
+++ app/models/categories_model.js
@@ -4,7 +4,7 @@ const category_model = {
     async getAllCategories() {
         try {
             const sqlQuery = {
-                text: `SELECT COUNT(posts.id) as posts_count, categories.name as category_name, categories.id AS id FROM posts JOIN categories ON category_id = categories.id group BY categories.name, categories.id`,
+                text: `SELECT COUNT(posts.id) as posts_count, categories.name as category_name, categories.id AS id FROM posts RIGHT JOIN categories ON category_id = categories.id group BY categories.name, categories.id`,
             };
             const response = await client.query(sqlQuery);
             return response.rows;
