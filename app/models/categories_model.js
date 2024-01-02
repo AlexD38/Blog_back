5366312ba8cca2d541bd349f64192661909ccb7f
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2024-01-02 23:12:56 +0100

    route categories send only cats id with names now

--- app/models/categories_model.js
+++ app/models/categories_model.js
@@ -4,21 +4,7 @@ const category_model = {
     async getAllCategories() {
         try {
             const sqlQuery = {
-                text: `SELECT
-  categories.id AS category_id,
-  categories.name AS category_name,
-  jsonb_agg(jsonb_build_object(
-    'title', posts.title,
-    'slug', posts.slug,
-    'body', posts.body
-  )) AS posts_data
-FROM
-  categories
-LEFT JOIN
-  posts ON categories.id = posts.category_id
-GROUP BY
-  categories.id, categories.name;
-`,
+                text: `SELECT * FROM categories`,
             };
             const response = await client.query(sqlQuery);
             return response.rows;
